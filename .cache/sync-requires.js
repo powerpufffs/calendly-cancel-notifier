1cc1538d56fc38ca7aafa49d6a1fdc2fe8e9b946
Z <asterixandisaac@gmail.com>
2020-02-03 11:59:08 -0700

    cache

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ztai/Code/calendly-cancel/calendly-cancel-notifier/.cache/dev-404-page.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ztai/Code/calendly-cancel/calendly-cancel-notifier/src/pages/index.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/calendly-cancel/calendly-cancel-notifier/.cache/dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/calendly-cancel/calendly-cancel-notifier/src/pages/index.js")))
 }
 
