c5c514b331e966777b708136a5e0a00ef60f01f3
Z <asterixandisaac@gmail.com>
2020-02-04 20:20:19 -0700

    fixed url error

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
 const ServerURL =
   process.env.MODE === "DEV"
     ? `http://localhost:9000/`
-    : `http://https://sleepy-wildwood-72675.herokuapp.com`
+    : `https://sleepy-wildwood-72675.herokuapp.com`
